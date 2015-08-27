d6c294595993805f150530ae1959603545df0c0a
scriptrdotio <git@apstrata.com>
2015-08-27 18:10:06 +0300

    Create config.js

---
+++ scripts/analytics/woopra/config.js
@@ -0,0 +1,18 @@
+// woopra track url 
+var woopra_http_tracking_url = "http://www.woopra.com/track/ce";
+// woopra identify url 
+var woopra_http_identify_url = "http://www.woopra.com/track/identify";
+var timeout="300000";
+
+
+function getTimeout() {
+  return timeout;
+}
+
+function getWoopraHttpTrackingURL() {
+  return woopra_http_tracking_url;
+}
+   	
+function getWoopraHttpIdentifyURL() {
+  return woopra_http_identify_url;
+}
