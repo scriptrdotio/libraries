f10bf641bd8c18476e02d61ac34f7023fc0c9f9e
Karim Saikali <karim.saikali@elementn.com>
2016-01-25 16:16:47 +0200

    Fixed bug on ws.open

--- clients/publish_subscribe/PubSubClient.js
+++ clients/publish_subscribe/PubSubClient.js
@@ -1,3 +1,5 @@
+var URL = "wss://api.scriptr.io/";
+
 /**
  * A simple web socket client to scriptr.io. 
  * Can handle multiple subscriptions to multiple channels.
@@ -5,12 +7,14 @@
  * @constructor PubSubClient
  * @param token: your scritpr.io auth token
  */
-function PubSubClient(token) {  
+function PubSubClient(token, url) {  
  
-	this.url = "wss://api.scriptr.io/" +  token;
+	this.url = url ? url + token :  URL + token;
 	this.subscriptions = {}; // list of channels subscriptions
 	this.publishWS = null; // this Web Socket connection is used for publishing only
 	this.ready = false; // status of publish Web Socket connection
+	this.connected = false;
+	this.publishMessageQueue = [];
 }
 
 /**
@@ -22,22 +26,36 @@ function PubSubClient(token) {
 PubSubClient.prototype.publish = function(channel, message) {
 	
 	var self = this;
-	
+	this.publishMessageQueue.push({
+		channel: channel,
+		message: message
+	});
+		
 	// Lazy initialization of the publish web socket connection
-	if (!this.publishWS || this.publishWS.readyState == 3) {
+	if (!this.publishWS || this.publishWS.readyState !== WebSocket.OPEN || !this.ready) {
 	
-		this.publishWS = new WebSocket(this.url);
+		if (!this.publishWS) {
+			this.publishWS = new WebSocket(this.url);
+		}	
+		
 		this.publishWS.onopen = function() {
+			
 			console.log("WS connection established for publishing");  
-		};
-		
+			self.connected = true;
+		}
+	
 		this.publishWS.onmessage = function(incoming) {
-		
+			
+			console.log("Incoming " + incoming.data);
 			if (!self.ready) {
+				
 				self.ready = true;
-				if (message) {
-					self.publishWS.send(JSON.stringify({"method": "Publish", "params":{"channel":channel, "message":message}}));
-				}
+				self._publish();									
+			}else {
+				
+				if (self.publishMessageQueue.length > 0) {
+					self.publishMessageQueue.splice(0,1);
+				}	
 			}
 		};
 		
@@ -53,7 +71,17 @@ PubSubClient.prototype.publish = function(channel, message) {
 			self.ready = false;
 		};
 	}else {
-		this.publishWS.send(JSON.stringify({"method": "Publish", "params":{"channel":channel, "message":message}}));
+		this._publish();
+	}
+};
+
+PubSubClient.prototype._publish = function() {
+	
+	for (var i = 0; i < this.publishMessageQueue.length; i++) {
+		
+		var msgObj = this.publishMessageQueue[i];
+		console.log("Sending " + JSON.stringify(msgObj));
+		this.publishWS.send(JSON.stringify({"method": "Publish", "params":{"channel":msgObj.channel, "message":msgObj.message}}));
 	}
 };
 
