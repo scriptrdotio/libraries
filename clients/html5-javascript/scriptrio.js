174b5c4735480a9150168d82ed332ff66f98e60d
Karim Saikali <karim.saikali@elementn.com>
2016-04-08 14:48:31 +0300

    Update scriptrio.js

--- clients/html5-javascript/scriptrio.js
+++ clients/html5-javascript/scriptrio.js
@@ -367,7 +367,7 @@ Scriptr.prototype._onHttpSuccess = function(responseObj, dto) {
 		}		
 	}catch(exception) {
 		
-		exception = (typeof exception == "object") ? JSON.stringify(exception) : exception;
+		exception = (typeof exception == "object" && exception.constructor.name == "Error") ? exception.message : JSON.stringify(exception);
 		var error = {
 			
 			errorCode: "Parsing_Error",
