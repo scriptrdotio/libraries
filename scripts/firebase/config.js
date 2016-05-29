d4530feaa1b8ed92c161cde776e1552288d68817
Ed Borden <borden.edward@gmail.com>
2016-05-28 22:26:49 -0400

    reset config

--- scripts/firebase/config.js
+++ scripts/firebase/config.js
@@ -1,2 +1,7 @@
-// your Firebase project name
-var apiUrl = 'PROJECT_NAME';
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ // your Firebase project name
+var projectName = '';			
