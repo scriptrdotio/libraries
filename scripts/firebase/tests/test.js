49ce9a8bf80d3763e57c2271c24a2d8c3172cee5
Ed Borden <borden.edward@gmail.com>
2016-05-28 22:25:00 -0400

    test 1 working

--- scripts/firebase/tests/test.js
+++ scripts/firebase/tests/test.js
@@ -1,28 +1,17 @@
-var c = require('initialstate/initialstateclient');
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var firebaseModule = require('firebase/firebaseclient');
+var firebase = new firebaseModule.Firebase();
 
-var cc = new c.InitialState();
-
-var bucket = {'bucketKey':'123','bucketName': 'bucket one two three'};
-var x;
+var test1;
 try {
-  x = cc.createBucket(bucket);
+  firebase.getData("locations");
+  test1 = "success";
 } catch (exception) {
-  x = exception;
+  test1 = exception;
 }
 
-var y;
-try {
-  y = cc.getVersions();
-} catch (exception) {
-  y = exception;
-}
-
-var events = [{'key': 'somekey', 'value': 'somevalue1'}, {'key': 'somekey', 'value': 'somevalue2'}];
-var z;
-try {
-  z = cc.sendEvents({'bucketKey': '123', 'events': events})
-} catch (exception) {
-  z = exception;
-}
-
-return {'versions':y, 'create bucket 123':x, 'send events somevalue1 and 2': z};
+return { "test getData": test1 };			
