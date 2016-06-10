63bab6914d32a075bc107913575483ff8410dc24
scriptrdotio <git@apstrata.com>
2016-06-10 11:47:17 +0300

    Added

---
+++ scripts/informatica/config.js
@@ -0,0 +1,9 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var app = "informatica";
+var username = "";
+var password = "";
+var defaultTaskType = "DSS";			
