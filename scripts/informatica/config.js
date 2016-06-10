f296530bfdd35f02e067dbb02bed16a4bdab0176
scriptrdotio <git@apstrata.com>
2016-06-10 17:59:28 +0300

    Delete config.js

--- scripts/informatica/config.js
+++
@@ -1,9 +0,0 @@
-/** Script ACLs do not delete 
- read=nobody 
-write=nobody
-execute=authenticated 
-  **/ 
- var app = "informatica";
-var username = "";
-var password = "";
-var defaultTaskType = "DSS";			
