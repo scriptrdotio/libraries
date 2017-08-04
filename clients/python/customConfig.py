5eb3508c330f41284e7ac981ff7f49be3d9a36f0
Wissam Assaf <wissam.assaf@elementn.com>
2017-08-04 09:25:01 +0300

    Update customConfig.py

--- clients/python/customConfig.py
+++ clients/python/customConfig.py
@@ -1,15 +1,15 @@
-websocket_url = "wss://api.scriptrapps.io"
-request_url = "https://wissam.scriptrapps.io"
-sec = 60
-method = "python/scheduleFromPython"
-token = "VDYzRkExMkUwNQ=="
-channel = "pubsub"
+websocket_url = ""
+request_url = ""
+sec = 0
+method = ""
+token = ""
+channel = ""
 
-
+#examples of callback functions
 def xx(message,*args):
     print( message)
 
 def x(message,*args):
     print("different function "+message)
-
-callback_map = {"1": [x,xx], "3": [xx]}
+#map of callback functions
+callback_map = {}
